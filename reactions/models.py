8307643b0b86802ba0d67b258ff37c8f72022c69
uppertoe <uppertoe@gmail.com>
2022-07-10 15:14:48 +1000

    bootstrap styling

--- reactions/models.py
+++ reactions/models.py
@@ -4,15 +4,22 @@ from django.urls import reverse
 
 
 # Create your models here.
+class LowercaseField(models.CharField):
+    def __init__(self, *args, **kwargs):
+        super(LowercaseField, self).__init__(*args, **kwargs)
+
+    def get_prep_value(self, value):
+        return str(value).lower()
+
 class DrugClass(models.Model):
-    name = models.CharField(max_length=255)
+    name =LowercaseField(max_length=255)
 
     def __str__(self):
         return self.name
 
 class Drug(models.Model):
-    name = models.CharField(max_length=255)
-    aliases = models.CharField(max_length=1023, blank=True)
+    name = LowercaseField(max_length=255, unique=True)
+    aliases = LowercaseField(max_length=1023, blank=True)
     drug_class = models.ManyToManyField(DrugClass, blank=True)
     slug = models.SlugField(null=False, unique=True)
 
@@ -23,8 +30,8 @@ class Drug(models.Model):
         return self.name
 
 class Condition(models.Model):
-    name = models.CharField(max_length=255)
-    aliases = models.CharField(max_length=1023, blank=True)
+    name = LowercaseField(max_length=255, unique=True)
+    aliases = LowercaseField(max_length=1023, blank=True)
     slug = models.SlugField(null=False, unique=True)
 
     def get_absolute_url(self):
@@ -70,7 +77,7 @@ class Interaction(models.Model):
         editable=False)
     condition = models.ForeignKey(Condition,on_delete=models.CASCADE)
     drugs = models.ManyToManyField(Drug)
-    reaction_type = models.CharField(max_length=255)
+    reaction_type = LowercaseField(max_length=255)
     description = models.TextField()
     severity = models.CharField(
         max_length=2,
@@ -81,8 +88,12 @@ class Interaction(models.Model):
         choices=evidence_choices,
         default=L7)
 
+    def get_drug_names(self):
+        return [drug.name for drug in self.drugs.all()]
+    
+    def __str__(self):
+        drugs_string = ' ,'.join(self.get_drug_names())
+        return f'{self.condition} and {drugs_string} interaction'
+
     def get_absolute_url(self):
         return reverse('interaction_detail', kwargs={'str': self.condition.slug, 'pk': str(self.id)})
-
-    def __str__(self):
-        return f'{self.condition} and {self.drugs} interaction'
