542563eb3e4b40a497ae8b739df63259dd12c1a4
uppertoe <uppertoe@gmail.com>
2022-07-13 20:54:28 +1000

    update hosts

--- neverdrugs/settings.py
+++ neverdrugs/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = env('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool('DJANGO_DEBUG', default=False)
-ALLOWED_HOSTS = ['django-never-drugs.herokuapp.com','localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['django-never-drugs.herokuapp.com','localhost', '127.0.0.1', 'neverdrugs.io']
 SECURE_SSL_REDIRECT = env.bool('DJANGO_SECURE_SSL_REDIRECT', default=True)
 SECURE_HSTS_SECONDS = env.int('DJANGO_SECURE_HSTS_SECONDS', default=2592000)
 SECURE_HSTS_INCLUDE_SUBDOMAINS = env.bool('DJANGO_SECURE_HSTS_INCLUDE_SUBDOMAINS', default=True)
