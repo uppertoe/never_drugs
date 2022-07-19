bf4df8ffe960c725e0b8d6fd322e0534bd9a4c6f
uppertoe <uppertoe@gmail.com>
2022-07-20 00:34:09 +1000

    adjust heroku yml

--- core/settings.py
+++ core/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 from environs import Env
+from django.core.management.utils import get_random_secret_key
 
 # Environs setup
 env = Env()
@@ -24,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env('DJANGO_SECRET_KEY')
+SECRET_KEY = env('DJANGO_SECRET_KEY', default=get_random_secret_key())
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool('DJANGO_DEBUG', default=False)
 ALLOWED_HOSTS = ['django-never-drugs.herokuapp.com','localhost', '127.0.0.1', 'www.neverdrugs.io']
