8a70091d572c1b3480e77625c81fff011198d373
uppertoe <uppertoe@gmail.com>
2022-08-07 02:33:30 +1000

    fix js navbar toggle

--- static/js/base.js
+++ static/js/base.js
@@ -1,22 +1,10 @@
-const navItems = document.querySelectorAll('.nav-link')
+let navlinks = document.querySelectorAll('.nav-link');
 
-// function for use in eventListener passing in event => e
-function setActive(e){
-  // get the parent of the event the UL
-  const parent = e.target.parentNode
-  // query the parent and get the active class
-  const active = parent.querySelector('.active')
-  // check for active class
-  if(active){
-    // toggle active class
-    active.classList.toggle('active')
-  }
-  // add active class
-  e.target.classList.add('active')
-}
-
-// forEach loop defining each node as the variable listItem
-navItems.forEach(listItem => {
-  // running click eventListener on each listItem with our setActive function
-  listItem.addEventListener("click", setActive)
-})
+navlinks.forEach((link) => {
+    // Add a click event on each link
+    link.addEventListener("click", () => {
+      let currActive = document.querySelector(".active");
+      currActive.classList.toggle("active");
+      link.classList.toggle("active");
+    });
+  });
