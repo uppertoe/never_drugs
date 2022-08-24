07d2ed8380f15a90a17291ff59604b03fee5d9ce
Eamonn Upperton <uppertoe@gmail.com>
2022-08-24 17:55:09 +1000

    implement review createview

--- review/forms.py
+++ review/forms.py
@@ -1,10 +1,13 @@
 from django.forms import ModelForm
 
 from reactions.models import Interaction
-from .models import ReviewSession
+from .models import ReviewSession, Review
 
 
 class InteractionForReviewForm(ModelForm):
+    '''
+    Filters fields available in the Session CreateView
+    '''
     class Meta:
         model = ReviewSession
         fields = ['interaction_reviews', 'host', 'open']
@@ -12,7 +15,7 @@ class InteractionForReviewForm(ModelForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.fields['interaction_reviews'].queryset = (
-            Interaction.objects
-            .filter(ready_for_peer_review=True)
-            .exclude(peer_review_status='AC')  # Exclude accepted articles
+            Review.objects
+            .filter(interaction__ready_for_peer_review=True)
+            .exclude(interaction__peer_review_status='AC')  # Exclude accepted articles
             )
