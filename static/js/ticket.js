fbdb58712ad99f1c6e8e68e3a95033fc7b91baa5
Eamonn Upperton <uppertoe@gmail.com>
2022-11-27 13:28:03 +1100

    remove staticfiles from docker image

--- static/js/ticket.js
+++ static/js/ticket.js
@@ -6,43 +6,44 @@ const spinner = document.createElement("span");
 // const ticket_url
 
 function dataSent() {
-    fieldset.setAttribute("disabled", "");
-    spinner.classList.add("spinner-border", "spinner-border-sm", "mx-2");
-    button.innerText = "Sending...";
-    button.prepend(spinner);
+  fieldset.setAttribute("disabled", "");
+  spinner.classList.add("spinner-border", "spinner-border-sm", "mx-2");
+  button.innerText = "Sending...";
+  button.prepend(spinner);
 }
 
 function responseReceived() {
-    spinner.remove();
-    button.classList.remove("btn-primary");
-    button.classList.add("btn-success");
-    button.innerText = "Sent";
+  spinner.remove();
+  button.classList.remove("btn-primary");
+  button.classList.add("btn-success");
+  button.innerText = "Sent";
 }
 
 function responseError() {
-    spinner.remove();
-    button.classList.remove("btn-primary");
-    button.classList.add("btn-danger");
-    button.innerText = "Failed to send";
+  spinner.remove();
+  button.classList.remove("btn-primary");
+  button.classList.add("btn-danger");
+  button.innerText = "Failed to send";
 }
 
-form.addEventListener('submit' ,(e)=>{
-  e.preventDefault();
-  const formData = new FormData(form);
-  dataSent();
+if (form) {
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const formData = new FormData(form);
+    dataSent();
 
-  fetch(ticket_url,{
-    method: 'post',
-    headers: {"X-Requested-With": "XMLHttpRequest"},
-    body:formData
-  })
-  .then((response)=> response.json())
-  .then((data)=> {
-    console.log(data);
-    responseReceived();
+    fetch(ticket_url, {
+      method: 'post',
+      headers: { "X-Requested-With": "XMLHttpRequest" },
+      body: formData
     })
-  .catch((error)=> {
-    console.error(error);
-    responseError();
+      .then((response) => response.json())
+      .then((data) => {
+        responseReceived();
+      })
+      .catch((error) => {
+        console.error(error);
+        responseError();
+      })
   })
-})
+}
